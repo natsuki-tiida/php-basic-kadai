241039c82d0679569d1db6a97915efe688e106d3
natsuki-tiida <ito@tiida-works.com>
2026-02-27 11:22:03 +0900

    7章課題をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>7章課題</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $vegetable_data = array(
+           "name" => "onion",
+           "price" => 200,
+           "weight" => 160);
+           print_r($vegetable_data);
+           ?>
+   </p>
+</body>
+
+</html>
